d1e2b928e040d63b0e5e601f97f00d564d76baca
Priya Karn <priya.karn.rn51@gmail.com>
2024-12-21 10:32:00 +0530

    updated day1.js file with practice first problem in js

---
+++ Day1/q1.js
@@ -0,0 +1,58 @@
+// const findLongestWord = (Str) => {
+//     if(Str.trim().length==0) return false;
+
+//     const arrStr = Str.split(" ");
+//     let res = "";
+
+//     for(const i of arrStr){
+//         if(res.length<i.length){
+//             res = i;
+//         }
+//     }
+    
+//     return res;
+// }
+
+
+// console.log(findLongestWord("worldbest javascriptprogramming language"))
+
+
+
+
+// another way using some predefined function :-
+
+// sort method
+// const findLongestWord = (Str) => {
+//     if(Str.trim().length==0) return false;
+
+//     const arrStr = Str.split(" ");
+//     const res = arrStr.sort((a,b)=>{
+//         return a.length - b.length       // number in ascending order
+//     })
+//     // return res[res.length-1]
+
+//     //or use at();
+
+//     return res.at(-1)
+// }
+
+
+// console.log(findLongestWord("world best javascript programming language"))
+
+
+// reduce method:- 
+
+const findLongestWord = (Str) => {
+    if(Str.trim().length==0) return false;
+
+    const arrStr = Str.split(" ");
+
+    return arrStr.reduce((acc,currWord,arr)=>{
+        return currWord.length>acc.length?currWord:acc
+    })
+
+ 
+}
+
+
+console.log(findLongestWord("world bestjavascript programming language"))
