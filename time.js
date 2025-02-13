fa42f3e5655037f2eea03b900564e10a2b6a23db
Priya Karn <priya.karn.rn51@gmail.com>
2025-02-13 23:22:08 +0530

    time count

---
+++ time.js
@@ -0,0 +1,12 @@
+const timeCount = ()=>{
+    let time = 0;
+   let res =  setInterval(()=>{
+        document.querySelector(".num").innerHTML = time;
+        time++;
+        if(time>10){
+            clearInterval(res);
+           }
+    },1000);  
+}
+
+timeCount();
